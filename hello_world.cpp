f070bfe36c4fa43950c382560e0ca53e31669c1f
STELLxFAS <egorkarbazov@gmail.com>
2026-03-16 23:02:14 +0000

    Add comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,13 @@
 #include <string>
 
 int main() {
+    // Запрашиваем имя пользователя
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
+    
+    // Выводим приветствие
     std::cout << "Hello world from " << name << std::endl;
-    return 0;
+    
+    return 0; // Программа завершена успешно
 }
