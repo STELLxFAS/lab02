f96a988c5301fa8ba6c40f42babb325cdc8e57b3
STELLxFAS <egorkarbazov@gmail.com>
2026-03-16 23:32:54 +0000

    Apply Mozilla code style with clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    // Get user name
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    
-    // Display greeting
-    std::cout << "Hello world from " << name << std::endl;
-    
-    return 0; // Program finished successfully
+int
+main()
+{
+  // Get user name
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+
+  // Display greeting
+  std::cout << "Hello world from " << name << std::endl;
+
+  return 0; // Program finished successfull
 }
+
